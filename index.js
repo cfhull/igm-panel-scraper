2ecd67f9289042bbe0502f408842df258f7c98e6
Chris Hull <chrishull@Verodins-MacBook-Pro-8.local>
2019-07-13 11:50:24 -0400

    adds colored logging

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const axios = require('axios')
 const fs = require('fs')
+const chalk = require('chalk')
 const jsdom = require('jsdom')
 const { JSDOM } = jsdom
 
-var url = 'http://www.igmchicago.org/igm-economic-experts-panel'
+const success = chalk.green
+const error = chalk.red
+
 if (!process.argv[2]) {
-  console.log("Cookie is required. Example: 'node index.js <cookie>' ")
+  console.log(error("Cookie is required. Example: 'node index.js <cookie>' "))
   process.exit(1)
 }
 const headers = {'Cookie': process.argv[2]}
@@ -16,10 +19,10 @@ const results = {
 }
 
 ;(async () => {
-	const body = (await axios({url, headers})).data
+	const body = (await axios({ url: 'http://www.igmchicago.org/igm-economic-experts-panel', headers})).data
 
 	if (body.includes('Incapsula')) {
-	  console.log('Cookie has expired')
+	  console.log(error('Cookie is invalid or has expired'))
     process.exit(1)
 	}
 
@@ -34,13 +37,22 @@ const results = {
 	doRequest(urls, 0)
 
 	function doRequest(urls, i) {
-		if (!urls[i]) {
-	    fs.writeFile("igmdata.json", JSON.stringify(results, null, 2), err => err ? console.log(err) : console.log("The file was saved!"))
-	    return
-		}
 		axios({ url: urls[i], headers}).then(body => {
-			scrapeData(body.data, urls[i])
-			doRequest(urls, i+1)
+			console.log(`[${i+1}/${urls.length}] ${urls[i]} -- ${scrapeData(body.data, urls[i]) ? success('Succeeded!') : error('Failed!')}`)
+
+		  if (urls[i+1]) {
+			  doRequest(urls, i+1)
+			} else {
+			  fs.writeFile("igmdata.json", JSON.stringify(results, null, 2), err => {
+	        if(err) {
+	          console.log(error(err))
+	        } else {
+	          console.log('\nResults written to ./igmdata.json')
+	          console.log('Errors: ', error(results.errors.length))
+	        }
+	      })
+	      return
+		  } 
 		})
 	}
 
@@ -53,9 +65,8 @@ const results = {
 	  if (window.document.querySelector('.post.post_domestic h2')) {
       topic = window.document.querySelector('.post.post_domestic h2').textContent
     } else {
-      console.log(url, '-- Failed!')
       results.errors.push({ url })
-      return
+      return false
     }
 	  const questions = Array.from(window.document.querySelectorAll('h3.surveyQuestion')).map(x => x.textContent.replace(/Question \w:/g, '').trim())
 
@@ -78,7 +89,7 @@ const results = {
 			  }))
 		  }))
 	  })
-		console.log(url, '-- Success!')
+	  return true
   }
 })()
 
